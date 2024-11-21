fdef9bd2c89c06f712c74d125b1537e9e7e10ad7
vinnietito <vincentnjuguna69@gmail.com>
2024-11-21 12:49:29 +0300

    Commits

--- collections.dart
+++ collections.dart
@@ -3,5 +3,14 @@ void demonstrateCollections() {
   List<String> list = ['Dart', 'Flutter', 'Programming'];
   list.add('OpenAi);
   list.remove('Programming');
-  print
+  print("List: $list");
+
+  //Set
+  Set<String> set = {'Dart', 'Flutter'};
+  set.add('Programing');
+  set.add('Dart'); //Duplicate won't be added
+  print("Set: $set");
+
+  //Map
+  Map<String, String
 }
