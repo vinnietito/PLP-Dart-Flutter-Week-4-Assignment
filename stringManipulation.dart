a0f830207f9da46d86c48a89a3e60fd404e0a193
vinnietito <vincentnjuguna69@gmail.com>
2024-11-21 11:36:44 +0300

    Commits

---
+++ stringManipulation.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Input string to manipulate
+  String input = "Dart";
+
+  // Calling the stringManipulation function with the input string
+  stringManipulation(input);
+}
+
+String reverseString(String input) {
+  return input.split('').reversed.join('');
+}
+
+void stringManipulation(String input) {
+  print("Original String: $input");
+  print("Concatenated: ${input + ' Dart is awesome!'}");
+  print("Interpolated: 'Welcome to $input programming!'");
+  print("Substring (0-4): ${input.substring(0, 4)}");
+  print("Uppercase: ${input.toUpperCase()}");
+  print("Lowercase: ${input.toLowerCase()}");
+  print("Reversed: ${reverseString(input)}");
+  print("Length: ${input.length}");
+}
