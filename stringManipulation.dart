d5f44af40cb31ba791dfbf0dda662bbf8f2406a5
vinnietito <vincentnjuguna69@gmail.com>
2024-11-21 12:20:35 +0300

    Commits

--- stringManipulation.dart
+++ stringManipulation.dart
@@ -8,9 +8,18 @@ void stringManipulation(String input) {
   print("Original Name: $input");
   print("Concatenated: ${input + ' is learning Dart!'}");
   print("Interpolated: 'Hello, $input! Welcome to the world of programming!'");
-  print("Substring(0-4): ${input.substring(0, 3)}");
+  print("Substring(0-3): ${input.substring(0, 3)}");
   print("Uppercase: ${input.toUpperCase()}");
   print("Lowercase: ${input.toLowerCase()}");
   print("Reversed: ${reverseString(input)}");
   print("Length: ${input.length}");
+}
+
+void main(){
+  //Requesting user input for their name
+  print("Please enter your name: ");
+  String input = stdin.readLineSync() ?? ""; //Reads input from the user, 
+
+  //Calling the stringManipulationfunction with the input string
+  stringManipulation(input);
 }
