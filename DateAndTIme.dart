620f3576ae6aaa50206c40726a477157301ff50e
vinnietito <vincentnjuguna69@gmail.com>
2024-11-21 13:37:38 +0300

    Commits

--- DateAndTIme.dart
+++ DateAndTIme.dart
@@ -1,21 +1,27 @@
 void dateAndTime() {
   DateTime now = DateTime.now();
+  print("Current Date and Time: $now");
 
-  //Formatting DateTime
+  // Formatting DateTime
   String formatted = "${now.year}-${now.month}-${now.day} ${now.hour}:${now.minute}";
   print("Formatted DateTime: $formatted");
 
-  //Parsing DateTime
+  // Parsing DateTime
   DateTime parsed = DateTime.parse("2024-12-25");
   print("Parsed Date: $parsed");
 
-  //Date Manipulation
+  // Date Manipulation
   DateTime future = now.add(Duration(days: 5));
-  DateTime past = now.substract(Duration(days: 10));
-  print("5 Days later: $future");
+  DateTime past = now.subtract(Duration(days: 10));
+  print("5 Days Later: $future");
   print("10 Days Earlier: $past");
 
-  //Diffrence between dates
-  Duration diffrence = future.difference(past);
-  print("Diffrence in Days: ")
+  // Difference between dates
+  Duration difference = future.difference(past);
+  print("Difference in Days: ${difference.inDays}");
+}
+
+void main() {
+  // Calling the dateAndTime function
+  dateAndTime();
 }
