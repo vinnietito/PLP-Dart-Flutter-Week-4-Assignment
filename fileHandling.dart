39d4b9540154b4a0431de71be98ca941324c296a
vinnietito <vincentnjuguna69@gmail.com>
2024-11-21 12:56:30 +0300

    Commits

---
+++ fileHandling.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void fileHandling() async {
+  try {
+    // Writing to a file
+    File writeFile = File('output.txt');
+    await writeFile.writeAsString('Dart File Handling Example\n');
+
+    // Reading from a file
+    File readFile = File('output.txt');
+    String content = await readFile.readAsString();
+    print("File Content: $content");
+  } catch (e) {
+    print("Error during file operation: $e");
+  }
+}
